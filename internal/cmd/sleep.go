1f41c88dc653aa429b086fb345dcdd9591045891
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2023-04-20 02:43:15 +0900

    sleep infinity

--- internal/cmd/sleep.go
+++ internal/cmd/sleep.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"context"
+	"os/signal"
 	"strconv"
 	"time"
 
@@ -9,22 +11,30 @@ import (
 
 func NewSleepCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "sleep [seconds]",
-		Short: "sleep",
+		Use:   "sleep [seconds|infinity]",
+		Short: "Sleep for a specified number of seconds or indefinitely",
 		Args: func(cmd *cobra.Command, args []string) error {
 			if err := cobra.ExactArgs(1)(cmd, args); err != nil {
 				return err
 			}
 
-			if _, err := strconv.Atoi(args[0]); err != nil {
-				return err
+			if args[0] != "infinity" {
+				if _, err := strconv.Atoi(args[0]); err != nil {
+					return err
+				}
 			}
 
 			return nil
 		},
 		Run: func(cmd *cobra.Command, args []string) {
-			sec, _ := strconv.Atoi(args[0])
-			time.Sleep(time.Second * time.Duration(sec))
+			if args[0] == "infinity" {
+				ctx, cancel := signal.NotifyContext(context.Background())
+				defer cancel()
+				<-ctx.Done()
+			} else {
+				sec, _ := strconv.Atoi(args[0])
+				time.Sleep(time.Second * time.Duration(sec))
+			}
 		},
 	}
 
